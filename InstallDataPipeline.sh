245671393d4ac745096d6b3293c5f781dffa6c30
leclue <leclue@gmail.com>
2014-03-15 11:58:50 +0000

    Adding a script that installs the AWS Data Pipeline tools on a standardAmazon Linux Instance

---
+++ InstallDataPipeline.sh
@@ -0,0 +1,40 @@
+#!/bin/bash --login
+
+#Intended for standard Amazon Linux AMI. Installs Data Pipeline CLI tools.
+
+# Install RVM
+\curl -sSL https://get.rvm.io | bash -s stable
+
+# Reload login scripts
+source /home/ec2-user/.profile 
+
+# Install ruby 1.9.3
+rvm install 1.9.3 
+
+# Get credentials
+echo "Provide AWS Access Key, followed by [ENTER]:"
+read access
+
+echo "Provide AWS Secret Key, followed by [ENTER]:"
+read secret
+
+# Activate New ruby version
+rvm use 1.9.3
+
+# Install Gems
+gem install uuidtools json httparty bigdecimal nokogiri
+
+# Download EDP CLI
+wget https://s3.amazonaws.com/datapipeline-us-east-1/software/latest/DataPipelineCLI/datapipeline-cli.zip 
+
+# Extract EDP CLI
+unzip datapipeline-cli.zip 
+
+#(insert credentials)
+echo '{
+  "access-id": "'$access'",
+  "private-key": "'$secret'"
+}' > /home/ec2-user/credentials.json 
+
+echo "Testing Data Pipeline CLI..."
+./datapipeline-cli/datapipeline --list-pipelines
